5944f03c826280c8d67e5e24929d1533cb4a32af
umerindented <umerfiaz251@gmail.com>
2022-11-05 14:48:48 -0600

    woring on prego

--- Dog Classifier'/Dog Classifier.py
+++ Dog Classifier'/Dog Classifier.py
@@ -7,6 +7,13 @@ class Dog:
   self.gender= gender #male/female
   self.prego = prego #self explanitory
   self.activity = activity #self explanitory
+  food_amount= ''
+  self.food_amount= food_amount
+
+ def food(self):
+    match self.food_amount:
+        case 
+
 
 
  def puppies(self):
@@ -72,7 +79,38 @@ class Dog:
                 return #food
 
  def senior(self):
-    if self.age == 'senior':
+    if self.age == 'senior' and self.gender== 'male':
+        if self.breed== 'toy':
+            if self.activity == 'not-active':
+                return #food
+            if self.activity == 'medium-activity':
+                return #food
+            if self.activity == 'active':
+                return #food
+        if self.breed == 'small':
+            if self.activity == 'not-active':
+                return #food
+            if self.activity == 'medium-activity':
+                return #food
+            if self.activity == 'active':
+                return #food
+        if self.breed == 'medium':
+            if self.activity == 'not-active':
+                return #food
+            if self.activity == 'medium-activity':
+                return #food
+            if self.activity == 'active':
+                return #food
+        if self.breed == 'large':
+            if self.activity == 'not-active':
+                return #food
+            if self.activity == 'medium-activity':
+                return #food
+            if self.activity == 'active':
+                return #food
+
+    if self.age == 'senior' and self.gender== 'female':
+        if self.pr
         if self.breed== 'toy':
             if self.activity == 'not-active':
                 return #food
