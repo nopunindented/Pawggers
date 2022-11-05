6826ad13eba7970f778bcbae2a9c9a94c9184dc9
umerindented <umerfiaz251@gmail.com>
2022-11-05 14:11:29 -0600

    "git push"

--- Dog Classifier'/Dog Classifier.py
+++ Dog Classifier'/Dog Classifier.py
@@ -1,18 +1,43 @@
 #start of dog classifier
 
 class Dog:
- def init(self, size, age, prego):
-  self.size = size
-  self.age = age
-  self.prego = prego
+ def __init__(self, breed, gender, age, prego, activity):
+  self.breed = breed #str for breed size
+  self.age = age #str fo range
+  self.gender= gender #male/female
+  self.prego = prego #self explanitory
+  self.activity = activity #self explanitory
 
- def food(self):
-  if self.age <= 8 and self.size == "toy":
-   ####
-   pass
-  elif self.age <= 11 and self.size == "small":
-   ####
-   pass
-  elif self.size in ['medium','large']:
-   ####
-   pass
+
+ def puppies(self):
+  if self.age == 'puppies':
+   if self.breed== 'toy':
+    if self.activity == 'not-active':
+     return #food
+    if self.activity == 'medium-activity':
+     return #food
+    if self.activity == 'active':
+     return #food
+   elif self.breed == 'small':
+    if self.activity == 'not-active':
+     return #food
+    if self.activity == 'medium-activity':
+     return #food
+    if self.activity == 'active':
+     return #food
+   elif self.breed == 'medium':
+    if self.activity == 'not-active':
+     return #food
+    if self.activity == 'medium-activity':
+        return #food
+    if self.activity == 'active':
+        return #food
+   elif self.breed == 'large':
+    if self.activity == 'not-active':
+        return #food
+    if self.activity == 'medium-activity':
+        return #food
+    if self.activity == 'active':
+        return #food
+  else: return None  
+     
