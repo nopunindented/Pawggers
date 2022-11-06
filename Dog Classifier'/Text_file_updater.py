9b55d7567115234b223dd903afe70de30823d1a4
LeLouLamb <112926148+LeLouLamb@users.noreply.github.com>
2022-11-06 01:58:27 -0700

    titties

---
+++ Dog Classifier'/Text_file_updater.py
@@ -0,0 +1,5 @@
+path_rel = "Pawggers/Dog Classifier'/PawMailer/PawMailer/bin/Debug/net6.0/"
+def times(when): (open(f"{path_rel}when.txt", "w"),[open(f"when.txt", "a").write(f"{x}") for x in when])[:]
+def users(email): open(f"{path_rel}who.txt", "w").write(f"{email}")
+def meals(food): open(f"{path_rel}food.txt", "w").write(f"Feed you dog {food} cups of food.")
+
